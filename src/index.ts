3db41fb83b07880783176340fd7e950dc0f0b20a
Cheryl Lee <clee0937@gmail.com>
2025-04-13 16:48:10 +0100

    added interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 import { getObstacleEvents } from "./computer-vision";
+
+interface AutonomousCar {
+  isRunning?: boolean;
+  respond: (events: Events) => void;
+}
+
+interface AutonomousCarProps {
+  isRunning?: boolean;
+}
+
+interface Events {
+  [obstacleDirection: string]: boolean;
+}
