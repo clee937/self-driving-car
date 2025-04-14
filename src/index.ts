2138ced69bdb187ddcd5104ee387492420e2dcfb
Cheryl Lee <clee0937@gmail.com>
2025-04-14 15:29:21 +0100

    added interfaces for Control and Steering

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,20 @@ interface AutonomousCar {
 
 interface AutonomousCarProps {
   isRunning?: boolean;
+  steeringControl: Steering;
 }
 
 interface Events {
   [obstacleDirection: string]: boolean;
 }
+
+// allow us to enforce that all controls for our cars must have an execute() method.
+interface Control {
+  execute: (command: string) => void;
+}
+
+interface Steering extends Control {
+  turn: (direction: string) => void;
+}
+
+console.log("hey");
